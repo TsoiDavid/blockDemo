e17367ce265a868d54e6eb42776fee4c743d6eeb
TsoiKaShing <cai306124949@sina.com>
2016-04-16 12:04:50 +0800

    branch 2.0 test

--- testDemo/testDemo/AppDelegate.m
+++ testDemo/testDemo/AppDelegate.m
@@ -13,7 +13,7 @@
 @end
 
 @implementation AppDelegate
-//test code branch code
+//test code branch code 2.0版
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
